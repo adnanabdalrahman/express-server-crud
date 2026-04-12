cbed3d7e3cb4113f07b6747e213f04dd177cf820
adnanabdalrahman <adnan.24150@gmail.com>
2026-04-12 03:45:00 +0200

    env

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ import pkg from "pg";
 
 const { Pool } = pkg;
 const pool = new Pool({
-  connectionString:
-    "postgresql://neondb_owner:npg_xf1q4HCTXoOP@ep-curly-hill-ai108uyk-pooler.c-4.us-east-1.aws.neon.tech/journal?sslmode=require&channel_binding=require",
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
